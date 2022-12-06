ee04fdf7efcfee91453cfc5945e55037c1f1012c
Futa Ikeda <futa.ikeda@gmail.com>
2022-12-06 15:03:11 -0500

    Reload app after removing self as moderator

--- lib/collections/addon/provider/moderation/moderators/controller.ts
+++ lib/collections/addon/provider/moderation/moderators/controller.ts
@@ -1,13 +1,9 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
-import RouterService from '@ember/routing/router-service';
-import { inject as service } from '@ember/service';
 
 export default class CollectionsModerationModeratorsController extends Controller {
-    @service router!: RouterService;
-
     @action
     afterSelfRemoval() {
-        this.transitionToRoute('provider.discover', this.model.id);
+        window.location.assign(`/collections/${this.model.id}/`);
     }
 }
--- lib/registries/addon/branded/moderation/moderators/controller.ts
+++ lib/registries/addon/branded/moderation/moderators/controller.ts
@@ -1,13 +1,9 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
-import RouterService from '@ember/routing/router-service';
-import { inject as service } from '@ember/service';
 
 export default class BrandedModerationModeratorsController extends Controller {
-    @service router!: RouterService;
-
     @action
     afterSelfRemoval() {
-        this.transitionToRoute('branded.discover', this.model.id);
+        window.location.assign(`/registries/${this.model.id}/`);
     }
 }
